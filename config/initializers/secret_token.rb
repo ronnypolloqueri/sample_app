319fc448cdc5c0a14101e906cc3c5ef761fc3335
Ronny Polloqueri <ronny.polloqueri@gmail.com>
2013-08-26 22:12:47 -0500

    Initial commit, con el tutorial de michaelHart, despues de haber configurado algunas gemas extra, agregado del token al .gitignore y haber instalado rspect

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,28 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+# SampleApp::Application.config.secret_key_base = 'f3a825ec824c45d7d67bbeed4f3bfcb482015ef43a5d54f6a107219f2c7971273f19f185b24c39a090d48979dc5dc95c685abb36138c528bdb237a3840b471ad'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
